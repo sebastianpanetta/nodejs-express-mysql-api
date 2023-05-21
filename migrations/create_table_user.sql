f79496e9f7f7f31b24419a52aaaa326807a58d62
sebastianpanetta <49350896+sebastianpanetta@users.noreply.github.com>
2023-05-21 20:33:25 -0300

    Added create table user SQL

---
+++ migrations/create_table_user.sql
@@ -0,0 +1,12 @@
+-- restapi_nodejs_express.`user` definition
+
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `surname` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`,`email`),
+  UNIQUE KEY `user_UN` (`email`),
+  KEY `user_id_IDX` (`id`) USING BTREE,
+  KEY `user_email_IDX` (`email`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
